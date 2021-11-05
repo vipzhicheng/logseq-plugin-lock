5cdf748c7e38709217b094f07bdf0af099d2e435
Richard Yu <vipzhicheng@gmail.com>
2021-11-05 19:50:13 +0800

    init: lock plugin

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import '@logseq/libs';
+
+import StegCloak from 'stegcloak';
+
+async function main() {
+
+
+  logseq.Editor.registerSlashCommand(
+    'lock',
+    async ({ uuid }) => {
+      // const {
+      //   left,
+      //   top,
+      //   rect,
+      // } = await logseq.Editor.getEditingCursorPosition();
+
+      // const passwordEl = document.getElementById('password');
+      // Object.assign(passwordEl.style, {
+      //   display: 'absolute',
+      //   top: top + rect.top + 'px',
+      //   left: left + rect.left + 'px',
+      // });
+      // logseq.showMainUI();
+      const { content } = await logseq.Editor.getBlock(uuid);
+      console.log('content', content);
+      const stegcloak = new StegCloak(true, false);
+      const lockedSecret = stegcloak.hide(content, 'password', 'locked secret');
+
+      logseq.Editor.updateBlock(uuid, `<a class="locked-secret" href="javascript:logseq.api.show_msg('Input /lock to get your secret.');" data-secret="${lockedSecret}">*</a>`);
+    },
+  );
+}
+
+logseq.ready(main).catch(console.error);
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    main: './src/main.ts',
+    style: './src/style.scss',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: './'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+        include: [
+          path.join(__dirname, './src'),
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+        include: [
+          path.join(__dirname, './src/styles'),
+          path.join(__dirname, './src/style.scss'),
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+    }),
+    new ESLintPlugin(),
+    new CleanWebpackPlugin({
+      verbose: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      chunks: ['style', 'main'],
+      chunksSortMode: 'manual',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/assets', to: 'assets' },
+        { from: './src/favicon.png', to: 'favicon.png' },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js'],
+    fallback: {
+      stream: require.resolve('stream-browserify'),
+      buffer: require.resolve('buffer/'),
+    },
+  },
+  externals: [],
+};
